555a736fb50b84e7f3057dd88680fcb925af235d
codingpalette <codingpalette@gmail.com>
2024-01-05 14:51:27 +0900

    content box 수정

--- src/app/_components/atoms/ContentBox.tsx
+++ src/app/_components/atoms/ContentBox.tsx
@@ -4,7 +4,7 @@
 export default function ContentBox({children}: {children: React.ReactNode}) {
   return (
     <>
-      <div className="relative max-w-5xl mx-auto mt-4">
+      <div className="relative max-w-7xl mx-auto mt-4">
         {children}
       </div>
     </>
